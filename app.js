ae780cb33211387a8575ee83d972d3f56da4c42a
MohammadrezaAflaki <am823617@gmail.com>
2020-08-05 11:06:17 +0430

    Level 1 and 2 username and password hashing

---
+++ app.js
@@ -0,0 +1,117 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const port = 4000;
+const app = express();
+
+//app.set("", path.join(__dirname, "views/partials"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public/views"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        if (_.toString(v).includes("@") && _.toString(v).includes(".")) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        if (_.toString(v).length > 6) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      message: (props) => `password must be at least 6 characters`,
+    },
+  },
+});
+
+const secret = "Thisismysecretkey";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = mongoose.model("user", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const userEmail = String(req.body.username).replace(" ", "");
+  const userPassword = req.body.password;
+  const newUser = new User({
+    email: userEmail,
+    password: userPassword,
+  });
+
+  User.findOne({ email: userEmail }, function (err, doc) {
+    if (!err) {
+      if (!doc || doc.length == 0) {
+        newUser.save();
+        console.log("user added");
+        res.render("secrets");
+      } else {
+        res.render("login");
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const userName = String(req.body.username).replace(" ", "");
+  const password = req.body.password;
+
+  User.findOne({ email: userName }, function (err, doc) {
+    if (!err) {
+      if (doc) {
+        if (doc.password === password) {
+          res.render("secrets");
+        } else {
+          res.render("login");
+        }
+      } else {
+        res.render("register");
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
